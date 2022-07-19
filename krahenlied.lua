a3cfcf13b9642539b4b6ca2ed4ac3eb3dcce7de8
William Hazard <105560469+williamthazard@users.noreply.github.com>
2022-07-18 20:27:22 -0400

    Update krahenlied.lua
    add title function + efficiency improvements through removal of unnecessary variables

--- krahenlied.lua
+++ krahenlied.lua
@@ -43,19 +43,11 @@
     s:settable(processString(text_string))
     j:settable(jfscaling(text_string))
   end
-  function text(str)
+  function title(str)
     text_string = str
     set()
-  end
-  function init()
-    input[1].mode('clock')
-    bpm = clock.tempo  
-    ii.jf.mode(1)
-    ii.jf.run_mode(1)
-    ii.jf.tick(bpm)
-    ii.wtape.timestamp(1)
-    ii.wtape.freq(0)
     ii.wtape.play(1)
+    ii.wtape.freq(0)
     coro_id = clock.run(notes_event)
               clock.run(other_event)
               clock.run(jfa_event)
@@ -76,185 +68,183 @@
               clock.run(loop_jumper)
               clock.run(tape_seeker)
   end
+  function text(str)
+    text_string = str
+    set()
+  end
+  function init()
+    input[1].mode('clock')
+    bpm = clock.tempo  
+    ii.jf.mode(1)
+    ii.jf.run_mode(1)
+    ii.jf.tick(bpm)
+    ii.wtape.timestamp(1)
+  end
   function notes_event()
     while true do
-        local notes_pitch = s()
-        local notes_time = s:step(2)()
-        local slew_step = s:step(3)()
-        local slew = slew_step/300
+        local notes_pitch = s()/12
+        local notes_time = s:step(2)()/s:step(3)()
+        local slew = s:step(4)()/300
         local notes_slope = { to(5,dyn{attack=1}/20), to(0,dyn{release=1}/20) }
-        local freq = notes_pitch/12
-        clock.sync(notes_time/4)
-        output[1].volts = freq
+        clock.sync(notes_time)
+        output[1].volts = notes_pitch
         output[2].action = notes_slope
-        output[2].dyn.attack = s:step(4)()
-        output[2].dyn.release = s:step(5)()
+        output[2].dyn.attack = s:step(5)()
+        output[2].dyn.release = s:step(6)()
         output[1].slew = slew
         output[2]()
     end
   end
   function other_event()
     while true do
-        local other_pitch = s:step(6)()
-        local other_time = s:step(7)()
-        local slew_step = s:step(8)()
-        local slew = slew_step/300
+        local other_pitch = s:step(7)()/12
+        local other_time = s:step(8)()/s:step(9)()
+        local slew = s:step(10)()/300
         local other_slope = { to(5,dyn{attack=1}/20), to(0,dyn{release=1}/20) }
-        local freq = other_pitch/12
-        clock.sync(other_time/4)
-        output[3].volts = freq
+        clock.sync(other_time)
+        output[3].volts = other_pitch
         output[4].action = other_slope
-        output[4].dyn.attack = s:step(9)()
-        output[4].dyn.release = s:step(10)()
+        output[4].dyn.attack = s:step(11)()
+        output[4].dyn.release = s:step(12)()
         output[3].slew = slew
         output[4]()
     end
   end
   function jfa_event()
     while true do
-        local jfa_pitch = s:step(11)()
-        local jfa_time = s:step(12)()
-        local freq = jfa_pitch/12
-        local level = j:step(13)()
-        clock.sync(jfa_time/8)
-        ii.jf.play_voice(1, freq, level)
+        local jfa_pitch = s:step(13)()/12
+        local jfa_time = s:step(14)()/s:step(15)()
+        local level = j:step(16)()
+        clock.sync(jfa_time)
+        ii.jf.play_voice(1, jfa_pitch, level)
     end
   end
   function jfb_event()
     while true do
-        local jfb_pitch = s:step(14)()
-        local jfb_time = s:step(15)()
-        local freq = jfb_pitch/12
-        local level = j:step(16)()
-        clock.sync(jfb_time/8)
-        ii.jf.play_voice(2, freq, level)
+        local jfb_pitch = s:step(17)()/12
+        local jfb_time = s:step(18)()/s:step(19)()
+        local level = j:step(20)()
+        clock.sync(jfb_time)
+        ii.jf.play_voice(2, jfb_pitch, level)
     end  
   end
   function jfc_event()
     while true do
-        local jfc_pitch = s:step(17)()
-        local jfc_time = s:step(18)()
-        local freq = jfc_pitch/12
-        local level = j:step(19)()
-        clock.sync(jfc_time/8)
-        ii.jf.play_voice(3, freq, level)
+        local jfc_pitch = s:step(21)()/12
+        local jfc_time = s:step(22)()/s:step(23)()
+        local level = j:step(24)()
+        clock.sync(jfc_time)
+        ii.jf.play_voice(3, jfc_pitch, level)
     end
   end
   function jfd_event()
     while true do
-        local jfd_pitch = s:step(20)()
-        local jfd_time = s:step(21)()
-        local freq = jfd_pitch/12
-        local level = j:step(22)()
-        clock.sync(jfd_time/8)
-        ii.jf.play_voice(4, freq, level)
+        local jfd_pitch = s:step(25)()/12
+        local jfd_time = s:step(26)()/s:step(27)()
+        local level = j:step(28)()
+        clock.sync(jfd_time)
+        ii.jf.play_voice(4, jfd_pitch, level)
     end
   end
   function jfe_event()
     while true do
-        local jfe_pitch = s:step(23)()
-        local jfe_time = s:step(24)()
-        local freq = jfe_pitch/12
-        local level = j:step(25)()
-        clock.sync(jfe_time/8)
-        ii.jf.play_voice(5, freq, level)
+        local jfe_pitch = s:step(29)()/12
+        local jfe_time = s:step(30)()/s:step(31)()
+        local level = j:step(32)()
+        clock.sync(jfe_time)
+        ii.jf.play_voice(5, jfe_pitch, level)
     end
   end
   function jff_event()
     while true do
-        local jff_pitch = s:step(26)()
-        local jff_time = s:step(27)()
-        local freq = jff_pitch/12
-        local level = j:step(28)()
-        clock.sync(jff_time/8)
-        ii.jf.play_voice(6, freq, level)
+        local jff_pitch = s:step(33)()/12
+        local jff_time = s:step(34)()/s:step(35)()
+        local level = j:step(36)()
+        clock.sync(jff_time)
+        ii.jf.play_voice(6, jff_pitch, level)
     end
   end
   function run_event()
     while true do
-        local runvolts = j:step(29)()
-        local runtime = s:step(30)()
+        local runvolts = j:step(37)()
+        local runtime = s:step(38)()
         clock.sync(runtime)
         ii.jf.run(runvolts)
     end
   end
   function quantize_event()
     while true do
-        local quant = s:step(30)()
-        local quantsync = s:step(31)()
+        local quant = s:step(39)()
+        local quantsync = s:step(40)()
         clock.sync(quantsync)
         ii.jf.quantize(quant)
     end
   end
   function with_event()
     while true do
-        local with_time = s:step(31)()
-        local speed_num = s:step(32)()
-        local speed_denom = s:step(33)()
+        local with_time = s:step(41)()/s:step(42)()
+        local speed_num = s:step(43)()
+        local speed_denom = s:step(44)()
         clock.sync(with_time)
         ii.wtape.speed(speed_num, speed_denom)
     end
   end
   function rev_event()
     while true do
-        local rev_time = s:step(34)()
+        local rev_time = s:step(45)()/s:step(46)()
         clock.sync(rev_time)
         ii.wtape.reverse()
     end
   end
   function loop_starter()
     while true do
-        local loop_starter_time = s:step(35)()
+        local loop_starter_time = s:step(47)()/s:step(48)()
         clock.sync(loop_starter_time)
         ii.wtape.loop_start()
     end
   end
   function loop_ender()
     while true do
-        local loop_ender_time = s:step(36)()
+        local loop_ender_time = s:step(49)()/s:step(50)()
         clock.sync(loop_ender_time)
         ii.wtape.loop_end()
     end
   end
   function loop_activator()
     while true do
-        local loop_activator_time = s:step(37)()
+        local loop_activator_time = s:step(51)()/s:step(52)()
         clock.sync(loop_activator_time)
         ii.wtape.loop_active(1)
     end
   end
   function loop_deactivator()
     while true do
-        local loop_deactivator_time = s:step(38)()
+        local loop_deactivator_time = s:step(53)()/s:step(54)()
         clock.sync(loop_deactivator_time)
         ii.wtape.loop_active(0)
     end
   end
   function loop_scaler()
     while true do
-        local loop_scaler_time = s:step(39)()
-        local scale_num = s:step(40)()
-        local scale_denom = s:step(41)()
-        local scale = scale_num/scale_denom
+        local loop_scaler_time = s:step(55)()
+        local scale = s:step(56)()/s:step(57)()
         clock.sync(loop_scaler_time)
         ii.wtape.loop_scale(scale)
     end
   end
   function loop_jumper()
     while true do
-        local loop_jumper_time = s:step(42)()
+        local loop_jumper_time = s:step(58)()
         clock.sync(loop_jumper_time)
         ii.wtape.loop_next()
     end
   end
   function tape_seeker()
     while true do
-        local seeker_time = s:step(43)()
-        local seek_time = s:step(44)()
-        local alternate_time = s:step(45)()
-        local big_seek_time = seek_time*300
-        local big_alternate_time = alternate_time*300
-        local big_time = big_seek_time-big_alternate_time
+        local seeker_time = s:step(59)()/s:step(60)()
+        local seek_time = s:step(61)()*300
+        local alternate_time = s:step(62)()*300
+        local big_time = seek_time-alternate_time
         clock.sync(seeker_time)
         ii.wtape.seek(big_time)
     end
