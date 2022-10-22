2fe31533d03a5230bfe40a276786e12b278a4cfe
William Hazard <105560469+williamthazard@users.noreply.github.com>
2022-10-22 17:40:54 -0400

    Update krahenlied.lua

--- krahenlied.lua
+++ krahenlied.lua
@@ -74,145 +74,137 @@
       clock.sync(s()/s:step(2)())
       output[1].volts = s:step(3)()/12
       output[1].slew = s:step(4)()/300
-      output[2].action = {to(5,dyn{attack=1}/20), to(3,dyn{decay=1}/20), to(3,dyn{sustain=1}/20), to(0,dyn{release=1}/20)}
-      output[2].dyn.attack = s:step(6)()
-      output[2].dyn.decay = s:step(7)()
-      output[2].dyn.sustain = s:step(8)()
-      output[2].dyn.release = s:step(9)()
+      output[2].action = ar(s:step(5)()/20, s:step(6)()/20, j:step(7)(), 'linear')
       output[2]()
     end
   end
   function other_event()
     while true do
-      clock.sync(s:step(10)()/s:step(11)())
-      output[3].volts = s:step(12)()/12
-      output[3].slew = s:step(13)()/300
-      output[4].action = {to(5,dyn{attack=1}/20), to(3,dyn{decay=1}/20), to(3,dyn{sustain=1}/20), to(0,dyn{release=1}/20)}
-      output[4].dyn.attack = s:step(15)()
-      output[4].dyn.decay = s:step(16)()
-      output[4].dyn.sustain = s:step(17)()
-      output[4].dyn.release = s:step(18)()
+      clock.sync(s:step(8)()/s:step(9)())
+      output[3].volts = s:step(10)()/12
+      output[3].slew = s:step(11)()/300
+      output[4].action = ar(s:step(12)()/20, s:step(13)()/20, j:step(14)(), 'linear')
       output[4]()
     end
   end
   function jfa_event()
     while true do
-      clock.sync(s:step(19)()/s:step(20)())
-      ii.jf.play_voice(1, s:step(21)()/12, j:step(22)())
+      clock.sync(s:step(15)()/s:step(16)())
+      ii.jf.play_voice(1, s:step(17)()/12, j:step(18)())
     end
   end
   function jfb_event()
     while true do
-      clock.sync(s:step(23)()/s:step(24)())
-      ii.jf.play_voice(2, s:step(25)()/12, j:step(26)())
+      clock.sync(s:step(19)()/s:step(20)())
+      ii.jf.play_voice(2, s:step(21)()/12, j:step(22)())
     end  
   end
   function jfc_event()
     while true do
-      clock.sync(s:step(27)()/s:step(28)())
-      ii.jf.play_voice(3, s:step(29)()/12, j:step(30)())
+      clock.sync(s:step(23)()/s:step(24)())
+      ii.jf.play_voice(3, s:step(25)()/12, j:step(26)())
     end
   end
   function jfd_event()
     while true do
-      clock.sync(s:step(31)()/s:step(32)())
-      ii.jf.play_voice(4, s:step(33)()/12, j:step(34)())
+      clock.sync(s:step(27)()/s:step(28)())
+      ii.jf.play_voice(4, s:step(29)()/12, j:step(30)())
     end
   end
   function jfe_event()
     while true do
-      clock.sync(s:step(35)()/s:step(36)())
-      ii.jf.play_voice(5, s:step(37)()/12, j:step(38)())
+      clock.sync(s:step(31)()/s:step(32)())
+      ii.jf.play_voice(5, s:step(33)()/12, j:step(34)())
     end
   end
   function jff_event()
     while true do
-      clock.sync(s:step(39)()/s:step(40)())
-      ii.jf.play_voice(6, s:step(41)()/12, j:step(42)())
+      clock.sync(s:step(35)()/s:step(36)())
+      ii.jf.play_voice(6, s:step(37)()/12, j:step(38)())
     end
   end
   function run_event()
     while true do
-      clock.sync(s:step(43)()/s:step(44)())
-      ii.jf.run(j:step(45)())
+      clock.sync(s:step(39)()/s:step(40)())
+      ii.jf.run(j:step(41)())
     end
   end
   function quantize_event()
     while true do
-      clock.sync(s:step(46)()/s:step(47)())
-      ii.jf.quantize(s:step(48)())
+      clock.sync(s:step(42)()/s:step(43)())
+      ii.jf.quantize(s:step(44)())
     end
   end
   function with_event()
     while true do
-      clock.sync(s:step(49)()/s:step(50)())
-      ii.wtape.speed(s:step(51)(), s:step(52)())
+      clock.sync(s:step(45)()/s:step(46)())
+      ii.wtape.speed(s:step(47)(), s:step(48)())
     end
   end
   function rev_event()
     while true do
-      clock.sync(s:step(53)()/s:step(54)())
+      clock.sync(s:step(49)()/s:step(50)())
       ii.wtape.reverse()
     end
   end
   function looper()
     while true do
-      clock.sync(s:step(55)()/s:step(56)())
+      clock.sync(s:step(51)()/s:step(52)())
       ii.wtape.loop_start()
-      clock.sync(s:step(57)()/s:step(58)())
+      clock.sync(s:step(53)()/s:step(54)())
       ii.wtape.loop_end()
-        if s:step(59)() < 17 then
-          for i = 1,j:step(60)() do 
-            clock.sync(s:step(61)()/s:step(62)())
-            ii.wtape.loop_scale(s:step(63)()/s:step(64)())
-            for i = 1,j:step(65)() do
-              clock.sync(s:step(66)()/s:step(67)())
-              ii.wtape.loop_next(s:step(68)()-s:step(69)())
+        if s:step(55)() < 17 then
+          for i = 1,j:step(56)() do 
+            clock.sync(s:step(57)()/s:step(58)())
+            ii.wtape.loop_scale(s:step(59)()/s:step(60)())
+            for i = 1,j:step(61)() do
+              clock.sync(s:step(62)()/s:step(63)())
+              ii.wtape.loop_next(s:step(64)()-s:step(65)())
             end 
           end
-        elseif s:step(59)() >= 17 then
-          for i = 1,j:step(70)() do
-            clock.sync(s:step(71)()/s:step(72)())
-            ii.wtape.loop_next(s:step(73)()-s:step(74)())
-            for i = 1,j:step(75)() do
-              clock.sync(s:step(76)()/s:step(77)())
-              ii.wtape.loop_scale(s:step(78)()/s:step(79)())
+        elseif s:step(55)() >= 17 then
+          for i = 1,j:step(66)() do
+            clock.sync(s:step(67)()/s:step(68)())
+            ii.wtape.loop_next(s:step(69)()-s:step(70)())
+            for i = 1,j:step(71)() do
+              clock.sync(s:step(72)()/s:step(73)())
+              ii.wtape.loop_scale(s:step(74)()/s:step(75)())
             end
           end
         end
-      clock.sync(s:step(80)()/s:step(81)())
+      clock.sync(s:step(76)()/s:step(77)())
       ii.wtape.loop_active(0)
-        for i = 1,s:step(82)() do
-          clock.sync(s:step(83)()/s:step(84)())
-          ii.wtape.seek((s:step(85)()*300)-(s:step(86)()*300))
+        for i = 1,s:step(78)() do
+          clock.sync(s:step(79)()/s:step(80)())
+          ii.wtape.seek((s:step(81)()*300)-(s:step(82)()*300))
         end
-        for i = 1,j:step(87)() do
-          clock.sync(s:step(88)()/s:step(89)())
+        for i = 1,j:step(83)() do
+          clock.sync(s:step(84)()/s:step(85)())
           ii.wtape.loop_active(1)
-          if s:step(90)() < 17 then
-            for i = 1,j:step(91)() do 
-              clock.sync(s:step(92)()/s:step(93)())
-              ii.wtape.loop_scale(s:step(94)()/s:step(95)())
-              for i = 1,j:step(96)() do
-                clock.sync(s:step(97)()/s:step(98)())
-                ii.wtape.loop_next(s:step(99)()-s:step(100)())
+          if s:step(86)() < 17 then
+            for i = 1,j:step(87)() do 
+              clock.sync(s:step(88)()/s:step(89)())
+              ii.wtape.loop_scale(s:step(90)()/s:step(91)())
+              for i = 1,j:step(92)() do
+                clock.sync(s:step(93)()/s:step(94)())
+                ii.wtape.loop_next(s:step(95)()-s:step(96)())
               end 
             end
-          elseif s:step(90)() >= 17 then
-            for i = 1,j:step(101)() do
-              clock.sync(s:step(102)()/s:step(103)())
-              ii.wtape.loop_next(s:step(104)()-s:step(105)())
-              for i = 1,j:step(106)() do
-                clock.sync(s:step(107)()/s:step(108)())
-                ii.wtape.loop_scale(s:step(109)()/s:step(110)())
+          elseif s:step(86)() >= 17 then
+            for i = 1,j:step(97)() do
+              clock.sync(s:step(98)()/s:step(99)())
+              ii.wtape.loop_next(s:step(100)()-s:step(101)())
+              for i = 1,j:step(102)() do
+                clock.sync(s:step(103)()/s:step(104)())
+                ii.wtape.loop_scale(s:step(105)()/s:step(106)())
               end
             end
           end
-          clock.sync(s:step(111)()/s:step(112)())
+          clock.sync(s:step(107)()/s:step(108)())
           ii.wtape.loop_active(0)
-          for i = 1,s:step(113)() do
-            clock.sync(s:step(114)()/s:step(115)())
-            ii.wtape.seek((s:step(116)()*300)-(s:step(117)()*300))
+          for i = 1,s:step(109)() do
+            clock.sync(s:step(110)()/s:step(111)())
+            ii.wtape.seek((s:step(112)()*300)-(s:step(113)()*300))
           end
         end
     end
